0fc6fb5bf58047108e0c866f2cb3b82d387f1aaa
munash48 <mugwanyan@gmail.com>
2019-08-04 16:15:44 +0300

    master page created

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -11,9 +11,38 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home","/index"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true );
 		
-		mv.addObject("greeting", "Welcome to spring mvc");
 		return mv;
 		
 	}
+	@RequestMapping(value = "/about")
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About");
+		mv.addObject("userClickAbout", true );
+		
+		return mv;
+		
+	}
+	@RequestMapping(value = "/listProducts")
+	public ModelAndView viewProducts(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Products");
+		mv.addObject("userClickViewProducts", true );
+		
+		return mv;
+		
+	}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true );
+		
+		return mv;
+		
+	}
+	
 }
